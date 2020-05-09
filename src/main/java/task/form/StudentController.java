eabd6296a6759522f06e7968fd137f8eb9fc1f12
danelekoo7 <danieljedrus@gmail.com>
2020-05-09 12:56:03 +0200

    * spring form

---
+++ src/main/java/task/form/StudentController.java
@@ -0,0 +1,46 @@
+package task.form;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.Arrays;
+import java.util.List;
+
+@Controller
+public class StudentController {
+
+    @ModelAttribute("hobbies")
+    public List<String> hobbiesOptions() {
+        String[] a = new String[] {"Programowanie", "rower", "muzyka","kino"};
+        return Arrays.asList(a);
+    }
+
+    @ModelAttribute("countryItems")
+    public List<String> countryOptions() {
+        String[] a = new String[] {"Polska", "USA", "Niemcy"};
+        return Arrays.asList(a);
+    }
+
+    @ModelAttribute("programmingSkills")
+    public List<String> programmingOptions() {
+        String[] a = new String[] {"Java", "JS", "Python", "RoR"};
+        return Arrays.asList(a);
+    }
+
+    @RequestMapping(value = "/new", method = RequestMethod.GET)
+    public String getForm(Model model){
+        model.addAttribute("student", new Student());
+        return "student";
+    }
+
+    @RequestMapping(value = "/new", method = RequestMethod.POST)
+    @ResponseBody
+    public Student createPerson(@ModelAttribute Student student){
+        return student;
+    }
+
+}
---
+++ src/main/webapp/WEB-INF/views/student.jsp
@@ -0,0 +1,41 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: daniel
+  Date: 09.05.2020
+  Time: 11:45
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<%@ taglib prefix="form" uri="http://www.springframework.org/tags/form" %>
+<html>
+<head>
+    <title>Title</title>
+</head>
+<body>
+<form:form method="POST" modelAttribute="student">
+    First Name: <form:input path="firstName"/>
+    <br>
+    Last Name: <form:input path="lastName"/>
+    <br>
+    gender Male: <form:radiobutton path="gender" value="male"/>
+    <br>
+    gender Female: <form:radiobutton path="gender" value="female"/>
+    <br>
+    country: <form:select path="country" items="${countryItems}"/>
+    <br>
+    notes: <form:textarea path="notes" rows="5" cols="30"/>
+    <br>
+    mailinglist: <form:checkbox path="mailingList"/>
+    <br>
+
+    programming Skills: <form:select path="programmingSkills" >
+        <form:options items="${programmingSkills}"/>
+        </form:select>
+    <br>
+    hobbies: <form:checkboxes items="${hobbies}" path="hobbies"/>
+
+    <input type="submit">
+</form:form>
+
+</body>
+</html>
